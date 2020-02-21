b72ddf1fdc6caea367708035a2eba32163611248
Michael <Michael>
2020-02-20 18:41:14 -0800

    Updated assembly test code

--- cpu/lab2_code.s
+++ cpu/lab2_code.s
@@ -10,8 +10,14 @@
 
 start:
 	mov r4, r5
+	mov r4, r5, LSL #2	@ test logical shift left
+	mov r4, r5, LSR #2	@ test logical shift right
+	mov r4, r5, ASR #2	@ test arithmetic shift right
+	mov r4, r5, ROR #2	@ test rotate right
 	mvn r4, r5
-	add r4, r5, r6
+	add r4, r6, r7
+	add r4, r6, #2	@ test use of immediate
+	add r4, r6, #261120	@test use of rotated immediate
 	sub r4, r5, r6
 	cmp r4, r5
 	tst r4, r5
@@ -19,5 +25,7 @@ start:
 	eor r4, r5, r6
 	bic r4, r5, r6
 	orr r4, r5, r6
-	ldr r4, [r5]
+	ldr r4, [r8]
+	ldr r4, [r8, #2]	@ test immediate offset
+	ldr r4, [r8, +r9, LSL #2]	@ test register shift offset
 	b start
