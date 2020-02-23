43d452f05845ecdb17022d5d2df7f94beed7f952
elutow <eloston@uw.edu>
2020-02-23 09:11:38 +0000

    Add top-level CPU module

--- cpu/cpu.sv
+++ cpu/cpu.sv
@@ -5,6 +5,12 @@
 
 `include "cpu/constants.svh"
 
+// NOTE: Yosys 0.9 doesn't support enums
+`define CPU_STATE_WIDTH 2
+`define CPU_STATE_RESET `CPU_STATE_WIDTH'd0
+`define CPU_STATE_START `CPU_STATE_WIDTH'd1
+`define CPU_STATE_RUN `CPU_STATE_WIDTH'd2
+
 module cpu(
         input wire clk,
         input wire nreset,
@@ -14,10 +20,12 @@ module cpu(
         /* verilator lint_on LITENDIAN */
     );
 
-    // Current instruction
-    logic [`BIT_WIDTH-1:0] inst_fetch, inst_decode, next_inst_fetch, next_inst_decode;
     // Program Counter
-    logic [`BIT_WIDTH-1:0] pc, next_pc;
+    // NOTE: In pipelined version, we want to update PC here.
+    logic [`BIT_WIDTH-1:0] pc;
+
+    // CPU state
+    logic [`CPU_STATE_WIDTH-1:0] ps, ns;
 
     // Turn on LED when reset is not on
     assign led = nreset;
@@ -25,35 +33,125 @@ module cpu(
     // TODO: Multicycle logic
     // TODO: We should assert that only one stage's ready signal is on at a time
     // for the multicycle only
+
+    // Shared modules
+
+    logic [`BIT_WIDTH-1:0] regfile_read_inst;
+    logic [`REG_COUNT_L2-1:0] regfile_read_addr1, regfile_read_addr2;
+    logic [`BIT_WIDTH-1:0] regfile_read_value1, regfile_read_value2;
+    logic regfile_write_enable1;
+    logic [`REG_COUNT_L2-1:0] regfile_write_addr1;
+    logic [`BIT_WIDTH-1:0] regfile_write_value1;
+    logic [`BIT_WIDTH-1:0] regfile_new_pc;
+    logic regfile_update_pc;
+    regfile the_regfile(
+        .clk(clk), .nreset(nreset), .read_inst(regfile_read_inst),
+        .read_addr1(regfile_read_addr1), .read_addr2(regfile_read_addr2),
+        .read_value1(regfile_read_value1), .read_value2(regfile_read_value2),
+        .write_enable1(regfile_write_enable1),
+        .write_addr1(regfile_write_addr1), .write_value1(regfile_write_value1),
+        .pc(pc), .new_pc(regfile_new_pc), .update_pc(regfile_update_pc)
+    );
+
+    // CPU stages
+
     logic fetcher_enable;
     logic fetcher_ready;
-    //fetcher the_fetcher(
-    //    .clk(clk), .nreset(nreset), .enable(fetcher_enable),
-    //    .ready(fetcher_ready))
+    logic [`BIT_WIDTH-1:0] fetcher_inst;
+    fetcher the_fetcher(
+        .clk(clk), .nreset(nreset), .enable(fetcher_enable),
+        .ready(fetcher_ready), .pc(pc), .fetcher_inst(fetcher_inst)
+    );
 
+    logic [`BIT_WIDTH-1:0] decoder_inst;
+    logic [`BIT_WIDTH-1:0] decoder_Rn_value, decoder_Rm_value;
+    logic decoder_ready;
+    decoder the_decoder(
+        .clk(clk), .nreset(nreset), .enable(fetcher_ready),
+        .ready(decoder_ready), .fetcher_inst(fetcher_inst),
+        .decoder_inst(decoder_inst), .regfile_read_inst(regfile_read_inst),
+        .regfile_read_addr1(regfile_read_addr1),
+        .regfile_read_addr2(regfile_read_addr2),
+        .regfile_read_value1(regfile_read_value1),
+        .regfile_read_value2(regfile_read_value2),
+        .Rn_value(decoder_Rn_value), .Rm_value(decoder_Rm_value)
+    );
+
+    logic executor_ready;
+    logic [`BIT_WIDTH-1:0] executor_inst;
+    logic executor_update_pc;
+    logic [`BIT_WIDTH-1:0] executor_new_pc;
+    logic executor_update_Rd;
+    logic [`BIT_WIDTH-1:0] executor_Rd_value;
+    executor the_executor(
+        .clk(clk), .nreset(nreset), .enable(decoder_ready),
+        .ready(executor_ready), .executor_inst(executor_inst),
+        .update_pc(executor_update_pc), .pc(pc), .new_pc(executor_new_pc),
+        .update_Rd(executor_update_Rd), .Rd_value(executor_Rd_value),
+        .decoder_inst(decoder_inst), .Rn_value(decoder_Rn_value),
+        .Rm_value(decoder_Rm_value)
+    );
+
+    logic regfilewriter_ready;
+    logic [`BIT_WIDTH-1:0] regfilewriter_new_pc;
+    logic regfilewriter_update_pc;
+    regfilewriter the_regfilewriter(
+        .clk(clk), .nreset(nreset), .enable(executor_ready),
+        .ready(regfilewriter_ready), .pc(pc), .executor_inst(executor_inst),
+        .update_pc(executor_update_pc),
+        .new_pc(executor_new_pc), .update_Rd(executor_update_Rd),
+        .Rd_value(executor_Rd_value),
+        .regfile_write_enable1(regfile_write_enable1),
+        .regfile_write_addr1(regfile_write_addr1),
+        .regfile_write_value1(regfile_write_value1),
+        .regfile_new_pc(regfilewriter_new_pc),
+        .regfile_update_pc(regfilewriter_update_pc)
+    );
+
+    // CPU FSM
+    always_comb begin
+        ns = ps;
+        case (ps)
+            `CPU_STATE_RESET: begin
+                ns = `CPU_STATE_START;
+            end
+            `CPU_STATE_START: begin
+                ns = `CPU_STATE_RUN;
+            end
+            `CPU_STATE_RUN: begin
+                // no-op
+            end
+            default: begin
+                `ifndef SYNTHESIS
+                    $error("Invalid CPU state: %d", ps);
+                `endif
+            end
+        endcase
+    end // comb
     always_ff @(posedge clk) begin
         if (nreset) begin
-            pc <= next_pc;
+            ps <= ns;
         end
         else begin
-            pc <= `BIT_WIDTH'b0;
+            ps <= `CPU_STATE_RESET;
         end
-    end
-
-    // TODO: We should move decoding to a seperate module that has a control
-    // signal indicating when the decoding is completely done.
-    // Also, have an input control signal that tells it to process the next instruction
+    end // ff
+    // Two situations to enable fetcher:
+    // - The CPU just started
+    // - Execution is done and PC is updated (regfilewriter_ready is asserted)
+    assign fetcher_enable = (ps == `CPU_STATE_START) || regfilewriter_ready;
+    // Debug port outputs
     always_comb begin
-        next_pc = pc + `BIT_WIDTH'd4;
         // Output to debug port
-        //debug_port_vector[1*8:5*8-1] = pc;
-        //debug_port_vector[5*8:6*8-1] = instruction_stage;
-//
-        //debug_port_vector[6*8:7*8-1] = regfile_read_addr1;
-        //debug_port_vector[7*8:11*8-1] = regfile_read_value1;
-        //debug_port_vector[11*8:12*8-1] = regfile_read_addr2;
-        //debug_port_vector[12*8:16*8-1] = regfile_read_value2;
-        //debug_port_vector[16*8:17*8-1] = regfile_write_addr1;
-        //debug_port_vector[17*8:21*8-1] = regfile_write_value1;
+        debug_port_vector[1*8:5*8-1] = pc;
+        debug_port_vector[5*8:6*8-1] = {
+            4'b0, fetcher_ready, decoder_ready, executor_ready, regfilewriter_ready};
+        debug_port_vector[6*8:7*8-1] = {4'b0, regfile_read_addr1};
+        debug_port_vector[7*8:11*8-1] = regfile_read_value1;
+        debug_port_vector[11*8:12*8-1] = {4'b0, regfile_read_addr2};
+        debug_port_vector[12*8:16*8-1] = regfile_read_value2;
+        debug_port_vector[16*8:17*8-1] = {4'b0, regfile_write_addr1};
+        debug_port_vector[17*8:21*8-1] = regfile_write_value1;
+        debug_port_vector[21*8:25*8-1] = fetcher_inst;
     end   // comb
 endmodule
--- cpu/regfilewriter.sv
+++ cpu/regfilewriter.sv
@@ -1,6 +1,7 @@
 // Writes back the result of executor to the regfile
 // Responsibilities:
 // - Updates the PC register with value from executor
+// - (NOTE: MULTICYCLE ONLY) If executor has no new PC value, then increment it normally (i.e. pc+4)
 // - Stores result of executor into the indicated register in regfile
 
 module regfilewriter(
@@ -9,13 +10,15 @@ module regfilewriter(
         input wire nreset,
         input logic enable,
         output logic ready,
+        // From regfile
+        input logic [`BIT_WIDTH-1:0] pc,
         // From executor
         input logic [`BIT_WIDTH-1:0] executor_inst,
         input logic update_pc, // Whether we have a new PC
         input logic [`BIT_WIDTH-1:0] new_pc,
         input logic update_Rd, // Whether we should update Rd (result) in writeback
         input logic [`BIT_WIDTH-1:0] Rd_value,
-        // Regfile I/O
+        // Regfile writing
         output logic regfile_write_enable1,
         output logic [`REG_COUNT_L2-1:0] regfile_write_addr1,
         output logic [`BIT_WIDTH-1:0] regfile_write_value1,
@@ -44,12 +47,12 @@ module regfilewriter(
     end // ff
 
     // Datapath logic
-    assign regfile_update_pc = next_ready && update_pc;
-    assign regfile_new_pc = new_pc;
     assign regfile_write_value1 = Rd_value;
     always_comb begin
         regfile_write_enable1 = 1'b0;
         regfile_write_addr1 = `REG_COUNT_L2'bX;
+        regfile_update_pc = 1'b0;
+        regfile_new_pc = `BIT_WIDTH'bX;
         if (next_ready && update_Rd) begin
             regfile_write_enable1 = 1'b1;
             if (decode_format(executor_inst) == `FMT_BRANCH
@@ -60,6 +63,23 @@ module regfilewriter(
             end
             else begin
                 regfile_write_addr1 = decode_Rd(executor_inst);
+            end
+        end
+        // Update PC
+        if (next_ready) begin
+            regfile_update_pc = 1'b1;
+            `ifndef SYNTHESIS
+                assert(!((regfile_write_addr1 == `REG_PC_INDEX) && update_pc)) else begin
+                    $error("Cannot set PC directly with write to PC simultaneously");
+                end
+            `endif
+            if (update_pc) begin
+                regfile_new_pc = new_pc;
+            end
+            else if (regfile_write_addr1 != `REG_PC_INDEX) begin
+                // NOTE: MULTICYCLE ONLY
+                // Update PC to next instruction otherwise
+                regfile_new_pc = pc + `BIT_WIDTH'd4;
             end
         end
     end // comb
