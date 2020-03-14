12ab24ccfa178d1626cd76f4188062c5eb0679e7
elutow <eloston@uw.edu>
2020-03-14 07:03:04 +0000

    decoder: Replace tabs with spaces, remove reigster for next instruction

--- cpu/decoder.sv
+++ cpu/decoder.sv
@@ -14,12 +14,12 @@ function automatic [1:0] decode_format;
             `FMT_DATA: begin end
             `FMT_MEMORY: begin end
             `FMT_BRANCH: begin
-				assert(inst[25]) else begin
-					$error("cached_inst[25] should be 1 for branch instructions.");
-				end
+                assert(inst[25]) else begin
+                    $error("cached_inst[25] should be 1 for branch instructions.");
+                end
             end
             default: begin
-				$error("Invalid instruction format");
+                $error("Invalid instruction format");
             end
         endcase
     `endif
@@ -114,21 +114,21 @@ function automatic [`REG_COUNT_L2-1:0] decode_Rm;
     input [`BIT_WIDTH-1:0] inst;
 
     `ifndef SYNTHESIS
-		case (decode_format(inst))
-			`FMT_DATA: begin
-				assert(!decode_dataproc_operand2_is_immediate(inst)) else begin
-					$error("decode_Rm: inst is dataproc format but uses immediate");
-				end
-			end
-			`FMT_MEMORY: begin
-				assert(!decode_mem_offset_is_immediate(inst)) else begin
-					$error("decode_Rm: inst is mem format but uses immediate");
-				end
-			end
-			default: begin
-	            $error("decode_Rm: inst is not in data or memory formats: %h", inst);
-			end
-		endcase
+        case (decode_format(inst))
+            `FMT_DATA: begin
+                assert(!decode_dataproc_operand2_is_immediate(inst)) else begin
+                    $error("decode_Rm: inst is dataproc format but uses immediate");
+                end
+            end
+            `FMT_MEMORY: begin
+                assert(!decode_mem_offset_is_immediate(inst)) else begin
+                    $error("decode_Rm: inst is mem format but uses immediate");
+                end
+            end
+            default: begin
+                $error("decode_Rm: inst is not in data or memory formats: %h", inst);
+            end
+        endcase
     `endif
 
     decode_Rm = inst[3:0];
@@ -161,7 +161,7 @@ function automatic decode_mem_up_down;
 endfunction
 
 function automatic decode_branch_is_link;
-	// 1 is branch with link, 0 is just branch
+    // 1 is branch with link, 0 is just branch
     input [`BIT_WIDTH-1:0] inst;
 
     `ifndef SYNTHESIS
@@ -182,7 +182,7 @@ function automatic [`BIT_WIDTH-1:0] decode_branch_offset;
         end
     `endif
 
-	// Extend two's complement to 32 bits
+    // Extend two's complement to 32 bits
     decode_branch_offset = {{6{inst[23]}}, inst[23:0], 2'b0};
 endfunction
 
@@ -202,48 +202,48 @@ function automatic [`BIT_WIDTH-1:0] fix_operand2_pc_read_value;
 endfunction
 
 module decoder(
-		input wire clk,
-		input wire nreset,
-		input logic enable,
-		// Whether decoder output is ready to be read
-		output logic ready,
-		// Instruction from fetcher
-		input logic [`BIT_WIDTH-1:0] fetcher_inst,
-		// Instruction decoding outputs
-		output logic [`BIT_WIDTH-1:0] decoder_inst,
-		// Regfile I/O
-		// regfile_read_addr* is determined directly from inst so we get the
-		//   result from regfile at the same clock cycle as cache_inst values
-		output logic [`BIT_WIDTH-1:0] regfile_read_inst,
-		output logic [`REG_COUNT_L2-1:0] regfile_read_addr1,
-		output logic [`REG_COUNT_L2-1:0] regfile_read_addr2,
-		input logic [`BIT_WIDTH-1:0] regfile_read_value1,
-		input logic [`BIT_WIDTH-1:0] regfile_read_value2,
-		output logic [`BIT_WIDTH-1:0] Rn_value, // First operand
-		output logic [`BIT_WIDTH-1:0] Rd_Rm_value // Rd for STR, otherwise Rm
-	);
+        input wire clk,
+        input wire nreset,
+        input logic enable,
+        // Whether decoder output is ready to be read
+        output logic ready,
+        // Instruction from fetcher
+        input logic [`BIT_WIDTH-1:0] fetcher_inst,
+        // Instruction decoding outputs
+        output logic [`BIT_WIDTH-1:0] decoder_inst,
+        // Regfile I/O
+        // regfile_read_addr* is determined directly from inst so we get the
+        //   result from regfile at the same clock cycle as cache_inst values
+        output logic [`BIT_WIDTH-1:0] regfile_read_inst,
+        output logic [`REG_COUNT_L2-1:0] regfile_read_addr1,
+        output logic [`REG_COUNT_L2-1:0] regfile_read_addr2,
+        input logic [`BIT_WIDTH-1:0] regfile_read_value1,
+        input logic [`BIT_WIDTH-1:0] regfile_read_value2,
+        output logic [`BIT_WIDTH-1:0] Rn_value, // First operand
+        output logic [`BIT_WIDTH-1:0] Rd_Rm_value // Rd for STR, otherwise Rm
+    );
 
-	// Control logic
-	// ---Decoding FSM---
-	// NOT READY
-	// - !enable -> NOT READY
-	// - enable -> READY
-	// 		- Tell regfile the new values to read
-	// READY
+    // Control logic
+    // ---Decoding FSM---
+    // NOT READY
+    // - !enable -> NOT READY
+    // - enable -> READY
+    //         - Tell regfile the new values to read
+    // READY
     // - enable -> READY (keep processing at 1 instruction / cycle)
-	// - !enable -> NOT READY (because we wait or get new instruction in this cycle)
+    // - !enable -> NOT READY (because we wait or get new instruction in this cycle)
     logic next_ready;
-	assign next_ready = enable;
-	always_ff @(posedge clk) begin
-		if (nreset) begin
-			ready <= next_ready;
-		end
-		else begin
-			ready <= 1'b0;
-		end
-	end // ff
+    assign next_ready = enable;
+    always_ff @(posedge clk) begin
+        if (nreset) begin
+            ready <= next_ready;
+        end
+        else begin
+            ready <= 1'b0;
+        end
+    end // ff
 
-	// Datapath logic
+    // Datapath logic
     // Read regfile read data and fix PC values
     // NOTE: These addresses can be Xs, but that doesn't matter since later
     // stages will read these values if the instruction uses them.
@@ -259,39 +259,34 @@ module decoder(
             Rd_Rm_value = fix_operand2_pc_read_value(regfile_read_value2, decoder_inst);
         end
     end // comb
-	// Determine instruction to output from decoder
-	logic [`BIT_WIDTH-1:0] next_decoder_inst;
-	always_comb begin
-		next_decoder_inst = decoder_inst;
-		if (next_ready) begin
-			next_decoder_inst = fetcher_inst;
-		end
-	end
-	always_ff @(posedge clk) begin
-		if (nreset) begin
-			decoder_inst <= next_decoder_inst;
-		end
-		else begin
-			decoder_inst <= `BIT_WIDTH'b0;
-		end
-	end
-	// Determine registers to be read from regfile
-	// NOTE: These operate on next_decoder_inst because we need the regfile to
-	// output on the same clock cycle as decoder_inst is set
-	assign regfile_read_inst = next_decoder_inst;
-	always_comb begin
-		regfile_read_addr1 = `REG_COUNT_L2'bX;
-		regfile_read_addr2 = `REG_COUNT_L2'bX;
-		if (next_ready) begin
-			case (decode_format(next_decoder_inst))
-				`FMT_DATA: begin
-					regfile_read_addr1 = decode_Rn(next_decoder_inst);
-					if (!decode_dataproc_operand2_is_immediate(next_decoder_inst)) begin
-						regfile_read_addr2 = decode_Rm(next_decoder_inst);
-					end
-				end
-				`FMT_MEMORY: begin
-					regfile_read_addr1 = decode_Rn(next_decoder_inst);
+    // Determine instruction to output from decoder
+    logic [`BIT_WIDTH-1:0] next_decoder_inst;
+    assign next_decoder_inst = fetcher_inst;
+    always_ff @(posedge clk) begin
+        if (nreset) begin
+            decoder_inst <= next_decoder_inst;
+        end
+        else begin
+            decoder_inst <= `BIT_WIDTH'b0;
+        end
+    end
+    // Determine registers to be read from regfile
+    // NOTE: These operate on next_decoder_inst because we need the regfile to
+    // output on the same clock cycle as decoder_inst is set
+    assign regfile_read_inst = next_decoder_inst;
+    always_comb begin
+        regfile_read_addr1 = `REG_COUNT_L2'bX;
+        regfile_read_addr2 = `REG_COUNT_L2'bX;
+        if (next_ready) begin
+            case (decode_format(next_decoder_inst))
+                `FMT_DATA: begin
+                    regfile_read_addr1 = decode_Rn(next_decoder_inst);
+                    if (!decode_dataproc_operand2_is_immediate(next_decoder_inst)) begin
+                        regfile_read_addr2 = decode_Rm(next_decoder_inst);
+                    end
+                end
+                `FMT_MEMORY: begin
+                    regfile_read_addr1 = decode_Rn(next_decoder_inst);
                     // We don't support three simultaneous regfile reads
                     // So we read Rd instead of Rm for STR only
                     if (!decode_mem_is_load(next_decoder_inst)) begin
@@ -302,19 +297,19 @@ module decoder(
                             end
                         `endif
                     end
-					else if (!decode_mem_offset_is_immediate(next_decoder_inst)) begin
-						regfile_read_addr2 = decode_Rm(next_decoder_inst);
-					end
-				end
-				`FMT_BRANCH: begin
-					// No need to read registers
-				end
-				default: begin
-					`ifndef SYNTHESIS
-						$error("This should not run!");
-					`endif
-				end
-			endcase
-		end
-	end // comb
+                    else if (!decode_mem_offset_is_immediate(next_decoder_inst)) begin
+                        regfile_read_addr2 = decode_Rm(next_decoder_inst);
+                    end
+                end
+                `FMT_BRANCH: begin
+                    // No need to read registers
+                end
+                default: begin
+                    `ifndef SYNTHESIS
+                        $error("This should not run!");
+                    `endif
+                end
+            endcase
+        end
+    end // comb
 endmodule
